969b7278d43d2380893d542cd4d0e8e7b08a3386
Anthony Powell <adpowell@mtu.edu>
2019-08-25 11:51:08 -0400

    Allow requests from other origins

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
+//Constants
 const PORT = process.env.API_PORT || 3000;
 
+// Middleware
+app.use(cors());
+
+// Export / Launch
 if (process.env.NODE_ENV === "production") {
   module.exports = app;
 } else {
